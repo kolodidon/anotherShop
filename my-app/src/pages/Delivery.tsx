ab084b1de0bd592ebf6a98b08b391f23225a5070
kolodidon <www.teteva@yandex.ru>
2022-02-28 12:47:12 +0300

    1. -дефолтные файлы, +роутинг и страницы

---
+++ my-app/src/pages/Delivery.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Delivery: React.FC = () => {
+  return (
+    <div>Delivery</div>
+  )
+}
+
+export default Delivery
---
+++ my-app/src/pages/index.tsx
@@ -0,0 +1,4 @@
+export {default as Cart} from './Cart';
+export {default as Delivery} from './Delivery';
+export {default as History} from './History';
+export {default as Home} from './Home';
